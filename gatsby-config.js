6dca7214d0c184265d62db797fb1e8775132f3cc
spences10 <spences10apps@gmail.com>
2018-03-23 14:29:05 +0000

    config plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,25 @@
+require('dotenv').config({
+  path: '.env.development'
+})
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-graphcms`,
+      options: {
+        endpoint: process.env.GRAPHCMS_ID,
+        token: process.env.GRAPHCMS_TOKEN,
+        query: `{
+          allArtists {
+            id
+            name
+          }
+        }`
+      }
+    }
+  ]
 }
