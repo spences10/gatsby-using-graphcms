219a16060d26f02b92078331b5d59740c5c11d0d
spences10 <spences10apps@gmail.com>
2018-03-27 18:00:53 +0100

    bring in page nav

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,66 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const path = require(`path`)
+// const queryAll = require(`./gatsby/queryAll.js`)
 
- // You can delete this file if you're not using it
+// exports.onCreateNode = ({ node }) => {
+//   console.log(`onCreateNode:`, node.internal.type)
+// }
+
+// exports.createPages = ({ boundActionCreators, graphql }) => {
+//   const { createPage } = boundActionCreators
+
+//   return new Promise((resolve, reject) => {
+//     const artistDetailPageTemplate = path.resolve(
+//       `./src/templates/artist-detail.js`
+//     )
+//     const recordDetailPageTemplate = path.resolve(
+//       `./src/templates/record-detail.js`
+//     )
+//     const reviewDetailPageTemplate = path.resolve(
+//       `./src/templates/review-detail.js`
+//     )
+
+//     resolve(
+//       graphql(queryAll).then(result => {
+//         if (result.errors) {
+//           reject(result.errors)
+//         }
+
+//         const artists = result.data.allArtist.edges
+//         artists.forEach(node => {
+//           const path = `artists/` + node.artist.slug
+//           createPage({
+//             path,
+//             component: artistDetailPageTemplate,
+//             context: {
+//               slug: node.artist.slug
+//             }
+//           })
+//         })
+
+//         const records = result.data.allRecord.edges
+//         records.forEach(node => {
+//           const path = `records/` + node.record.slug
+//           createPage({
+//             path,
+//             component: recordDetailPageTemplate,
+//             context: {
+//               slug: node.record.slug
+//             }
+//           })
+//         })
+
+//         const reviews = result.data.allReview.edges
+//         reviews.forEach(node => {
+//           const path = `reviews/` + node.review.slug
+//           createPage({
+//             path,
+//             component: reviewDetailPageTemplate,
+//             context: {
+//               slug: node.review.slug
+//             }
+//           })
+//         })
+//       })
+//     )
+//   })
+// }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,31 +5,54 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header'
 import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const TemplateWrapper = ({ children, data }) => {
+  const { edges: navItems } = data.allNavigationLinks
+  return (
+    <div>
+      <Helmet
+        title="Gatsby Default Starter"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' }
+        ]}
+      />
+      <Header navItems={console.log(navItems)} />
+
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0
+        }}>
+        {children()}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func
 }
 
 export default TemplateWrapper
+
+export const query = graphql`
+  query NavItems {
+    allNavigationLinks {
+      edges {
+        node {
+          id
+          name
+          parentNavLink {
+            id
+          }
+          subNavLinks {
+            id
+          }
+          topLevelNavItem
+        }
+      }
+    }
+  }
+`
