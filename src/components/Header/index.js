9fd48bb9a09af536c041c4c230914f1b20c6202f
spences10 <spences10apps@gmail.com>
2018-03-28 12:16:14 +0100

    Prettying

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Header = () => (
+import PageNav from './PageNav'
+
+const Header = props => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
+      marginBottom: '1.45rem'
+    }}>
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+        padding: '1.45rem 1.0875rem'
+      }}>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+            textDecoration: 'none'
+          }}>
           Gatsby
         </Link>
+        <PageNav nav={props} />
       </h1>
     </div>
   </div>
