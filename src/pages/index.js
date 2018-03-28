3d6516369fd40f112d96d5236e69f8874e2a48f9
spences10 <spences10apps@gmail.com>
2018-03-28 12:16:45 +0100

    query for nav items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query getAllNavigationItems {
+    links: allNavigationLinks {
+      edges {
+        node {
+          id
+          name
+          parentNavLink {
+            id
+          }
+          subNavLinks {
+            id
+          }
+          topLevelNavItem
+        }
+      }
+    }
+  }
+`
