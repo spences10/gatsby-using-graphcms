8f083c9d261502a58739cd254e14ae3ac3144d5d
spences10 <spences10apps@gmail.com>
2018-04-03 16:19:23 +0100

    style basic page

--- src/pages/guidance.js
+++ src/pages/guidance.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+import { columns } from '../theme/globalStyle'
+
+const PageWrapper = styled.div`
+  display: grid;
+  grid-area: m;
+  grid-template-columns: repeat(${columns.giant}, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas: '. pc pc pc pc pc pc pc pc pc pc .';
+`
+
+const PageContent = styled.div`
+  grid-area: pc; /* Guidance Content */
+`
 
 const GuidancePage = () => (
-  <div>
-    <h1>Hi from the Guidance page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+  <PageWrapper>
+    <PageContent>
+      <h1>Hi from the Guidance page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </PageContent>
+  </PageWrapper>
 )
 
 export default GuidancePage
