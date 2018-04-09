92595e6d1b8e19574698b780139dc84d3ac36462
spences10 <spences10apps@gmail.com>
2018-04-09 15:13:59 +0100

    indent

--- gatsby/queryAll.js
+++ gatsby/queryAll.js
@@ -1,78 +1,78 @@
 'use strict'
 
 module.exports = `
-{
-  allAssets {
-    edges {
-      node {
-        id
-        size
-        url
-        height
-        width
-        fileName
-        mimeType
-      }
-    }
-  }
-
-  allBlogPost {
-	  edges {
-	    node {
-	      id
-        body
-        createdAt
-	    }
-	  }
-	}
-
-  allEvent {
-    edges {
-      node {
-        id
-        title
-      }
-    }
-  }
-
-  allNavigationLink {
-    edges {
-      node {
-        id
-        name
-        topLevelNavItem
-        parentNavLink {
+  {
+    allAssets {
+      edges {
+        node {
           id
-          name
-        }
-        subNavLinks {
-          id
-          name
-        }
-      }
-    }
-  }
-
-  allPage {
-    edges {
-      node {
-        id
-        id
-        headerImage {
-          id
-          fileName
+          size
           url
+          height
+          width
+          fileName
+          mimeType
         }
-        isPublished
-        pageName
-        pageDescription
-        pageNameSlug
-        pageTitle
       }
     }
-  }
 
-}
+    allBlogPost {
+      edges {
+        node {
+          id
+          body
+          createdAt
+        }
+      }
+    }
+
+    allEvent {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+
+    allNavigationLink {
+      edges {
+        node {
+          id
+          name
+          topLevelNavItem
+          parentNavLink {
+            id
+            name
+          }
+          subNavLinks {
+            id
+            name
+          }
+        }
+      }
+    }
+
+    allPage {
+      edges {
+        node {
+          id
+          id
+          headerImage {
+            id
+            fileName
+            url
+          }
+          isPublished
+          pageName
+          pageDescription
+          pageNameSlug
+          pageTitle
+        }
+      }
+    }
+
+  }
 `
 
 // query NavLinks ($topLevel: Boolean!) {
