e262e057b03669b7ed64842124919f10c8932704
spences10 <spences10apps@gmail.com>
2018-04-09 14:55:53 +0100

    add configQuery queries

--- gatsby/configQuery.js
+++ gatsby/configQuery.js
@@ -1,4 +1,77 @@
+'use strict'
+
 module.exports = `
+{
+  allAsset {
+    edges {
+      node {
+        id
+        size
+        url
+        height
+        width
+        fileName
+        mimeType
+      }
+    }
+  }
+
+  allBlogPost {
+	  edges {
+	    node {
+	      id
+        body
+        createdAt
+	    }
+	  }
+	}
+
+  allEvent {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+
+  allNavigationLink {
+    edges {
+      node {
+        id
+        name
+        topLevelNavItem
+        parentNavLink {
+          id
+          name
+        }
+        subNavLinks {
+          id
+          name
+        }
+      }
+    }
+  }
+
+  allPage {
+    edges {
+      node {
+        id
+        id
+        headerImage {
+          id
+          fileName
+          url
+        }
+        isPublished
+        pageName
+        pageDescription
+        pageNameSlug
+        pageTitle
+      }
+    }
+  }
+
   query NavLinks ($topLevel: Boolean!) {
     allNavigationLink (filter: {topLevelNavItem:$topLevel}){
       name
@@ -7,4 +80,6 @@ module.exports = `
       }
     }
   }
+
+}
 `
