346d4c85ea7cae40e1a75ee7cd8906438b9a6a10
spences10 <spences10apps@gmail.com>
2018-10-16 11:19:05 +0100

    code mod

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32,7 +32,7 @@ export const slugIt = text => {
     .replace(/[^\w\-]+/g, '') // Remove all non-word chars
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
+    .replace(/-+$/, ''); // Trim - from end of text
 }
 
 // https://github.com/mobz/lag-radar/blob/master/lag-radar.js
