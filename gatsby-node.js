f46718f539c4072b60eb6f233dc53da991bebb71
spences10 <spences10apps@gmail.com>
2018-12-04 19:45:38 +0000

    remove this for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,78 +1,78 @@
-const path = require(`path`)
-const queryAll = require(`./gatsby/queryAll.js`)
+// const path = require(`path`)
+// const queryAll = require(`./gatsby/queryAll.js`)
 
-exports.onCreateNode = ({ node }) => {
-  console.log(`onCreateNode:`, node.internal.type)
-}
+// exports.onCreateNode = ({ node }) => {
+//   console.log(`onCreateNode:`, node.internal.type)
+// }
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const pageDetailTemplate = path.resolve(
-      `./src/templates/pageDetail.js`
-    )
+//   return new Promise((resolve, reject) => {
+//     const pageDetailTemplate = path.resolve(
+//       `./src/templates/pageDetail.js`
+//     )
 
-    resolve(
-      graphql(queryAll).then(result => {
-        if (result.errors) {
-          reject(result.errors)
-        }
+//     resolve(
+//       graphql(queryAll).then(result => {
+//         if (result.errors) {
+//           reject(result.errors)
+//         }
 
-        // console.log('=====================')
-        // console.log(result.data.allPage.edges)
-        // console.log('=====================')
+//         // console.log('=====================')
+//         // console.log(result.data.allPage.edges)
+//         // console.log('=====================')
 
-        const pages = result.data.allPage.edges
-        pages.map(({ node: page }) => {
-          const path = `/${page.pageNameSlug}`
-          // console.log('=====================')
-          // console.log(path)
-          // console.log('=====================')
-          createPage({
-            path,
-            component: pageDetailTemplate,
-            context: {
-              slug: page.pageNameSlug
-            }
-          })
-        })
+//         const pages = result.data.allPage.edges
+//         pages.map(({ node: page }) => {
+//           const path = `/${page.pageNameSlug}`
+//           // console.log('=====================')
+//           // console.log(path)
+//           // console.log('=====================')
+//           createPage({
+//             path,
+//             component: pageDetailTemplate,
+//             context: {
+//               slug: page.pageNameSlug
+//             }
+//           })
+//         })
 
-        // pages.forEach(node => {
-        //   const path = `/` + node.page.slug
-        //   createPage({
-        //     path,
-        //     component: pageDetailTemplate,
-        //     context: {
-        //       slug: node.page.slug
-        //     }
-        //   })
-        // })
+//         // pages.forEach(node => {
+//         //   const path = `/` + node.page.slug
+//         //   createPage({
+//         //     path,
+//         //     component: pageDetailTemplate,
+//         //     context: {
+//         //       slug: node.page.slug
+//         //     }
+//         //   })
+//         // })
 
-        // const records = result.data.allRecord.edges
-        // records.forEach(node => {
-        //   const path = `records/` + node.record.slug
-        //   createPage({
-        //     path,
-        //     component: recordDetailPageTemplate,
-        //     context: {
-        //       slug: node.record.slug
-        //     }
-        //   })
-        // })
+//         // const records = result.data.allRecord.edges
+//         // records.forEach(node => {
+//         //   const path = `records/` + node.record.slug
+//         //   createPage({
+//         //     path,
+//         //     component: recordDetailPageTemplate,
+//         //     context: {
+//         //       slug: node.record.slug
+//         //     }
+//         //   })
+//         // })
 
-        // const reviews = result.data.allReview.edges
-        // reviews.forEach(node => {
-        //   const path = `reviews/` + node.review.slug
-        //   createPage({
-        //     path,
-        //     component: reviewDetailPageTemplate,
-        //     context: {
-        //       slug: node.review.slug
-        //     }
-        //   })
-        // })
-      })
-    )
-  })
-}
+//         // const reviews = result.data.allReview.edges
+//         // reviews.forEach(node => {
+//         //   const path = `reviews/` + node.review.slug
+//         //   createPage({
+//         //     path,
+//         //     component: reviewDetailPageTemplate,
+//         //     context: {
+//         //       slug: node.review.slug
+//         //     }
+//         //   })
+//         // })
+//       })
+//     )
+//   })
+// }
