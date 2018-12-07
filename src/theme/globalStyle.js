b9148aa53f1e56da80b5dbdfe421da0430935f8f
spences10 <spences10apps@gmail.com>
2018-12-07 21:14:33 +0000

    simple style

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,62 +1,17 @@
-import { createGlobalStyle, css } from 'styled-components'
-
-export const theme1 = {
-  primary: '#051d49',
-  secondary: '#01c1d6',
-  inset: '#571d50',
-  danger: '#eb238e',
-  light: '#f4f4f4',
-  dark: '#222'
-}
-
-export const theme2 = {
-  primary: '#6e27c5',
-  secondary: '#ffb617',
-  danger: '#f16623',
-  light: '#f4f4f4',
-  dark: '#222'
-}
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,900|Roboto');
-
+  @import url('https://fonts.googleapis.com/css?family=Kodchasan:400,700');
   body {
     padding: 0;
     margin: 0;
-    font-family: Roboto, sans-serif;
+    font-family: Kodchasan;
   }
-
-  h1 {
-    font-family: Montserrat;
+  a {
+    text-decoration: none;
+  }
+  ul {
+    margin: 0 auto;
+    list-style-type: none;
   }
 `
-
-export const columns = {
-  giant: 12,
-  desktop: 10,
-  tablet: 8,
-  phone: 6
-}
-
-const sizes = {
-  giant: 1170,
-  desktop: 992,
-  tablet: 768,
-  phone: 376
-}
-
-// iterate through the sizes and create a media template
-export const media = Object.keys(sizes).reduce(
-  (accumulator, label) => {
-    // use em in breakpoints to work properly cross-browser and support users
-    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-    const emSize = sizes[label] / 16
-    accumulator[label] = (...args) => css`
-      @media (max-width: ${emSize}em) {
-        ${css(...args)};
-      }
-    `
-    return accumulator
-  },
-  {}
-)
